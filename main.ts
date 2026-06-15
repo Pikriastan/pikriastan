5f277eb01cbc246b6bac12588bf9354e6b443417
Giorgi Merebashvili <mereba2627@gmail.com>
2026-06-15 23:30:04 +0400

    Add caching fonts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
 import type { State } from "@/lib/utils.ts";
 import { App, staticFiles } from "fresh";
 
-export const app = new App<State>().use(staticFiles()).fsRoutes();
+const FONT_CACHE_CONTROL = "public, max-age=31536000, immutable";
+
+export const app = new App<State>()
+  .use(async (ctx) => {
+    const response = await ctx.next();
+
+    if (ctx.url.pathname.startsWith("/fonts/")) {
+      response.headers.set("Cache-Control", FONT_CACHE_CONTROL);
+    }
+
+    return response;
+  })
+  .use(staticFiles())
+  .fsRoutes();
